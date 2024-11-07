4e544d46681be0b886e9db0e2058bf242682ee2c
haim rosenberg <haimzviroz@gmail.com>
2024-11-07 09:48:44 +0200

    3.1 finished

--- src/store/floorreducer.ts
+++ src/store/floorreducer.ts
@@ -1,1 +1,25 @@
-//FILL HERE 3.1
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface FloorState {
+  floors: boolean[];
+}
+
+const initialState: FloorState = {
+  floors: [false, false, false, false, false],
+};
+
+const floorSlice = createSlice({
+  name: "floor",
+  initialState,
+  reducers: {
+    changeAccessByindex: (state, action: PayloadAction<number>) => {
+      const floorIndex = action.payload;
+      if (floorIndex >= 0 && floorIndex < state.floors.length) {
+        state.floors[floorIndex] = !state.floors[floorIndex];
+      }
+    },
+  },
+});
+
+export const { changeAccessByindex: changeAccess } = floorSlice.actions;
+export default floorSlice.reducer;
